616766370f97bb94c614d7b4b491d84614c37840
orlanddoneto <neto020735@gmail.com>
2023-10-04 01:05:52 -0300

    busca com unico termo finalizada

--- motor.py
+++ motor.py
@@ -1,58 +1,71 @@
 import xml.etree.ElementTree as ET
+from collections import defaultdict
 tree = ET.parse("D:\\Usuario\\Cliente\\Downloads\\verbetesWikipedia.xml")
 root = tree.getroot()
+cache = {}
+arrayTablesPages = []
 
-def count_occurrences (name, page):
-    count = 0
+def get_table_page (page):
+    tablePage = defaultdict(int)
+
     for wordInText in page[2].text.split(" "):
-        if(name.upper() == wordInText.upper()):
-            count +=1
+        if(len(wordInText) > 3):
+            tablePage[wordInText.upper()] += 1
+
     for wordInTitle in page[1].text.split(" "):
-        if (name.upper() == wordInTitle.upper()):
-            count += 10
+        if (len(wordInTitle) > 3):
+            tablePage[wordInTitle.upper()] += 10
 
-    return count
+    return dict(tablePage)
 
 
-def printResults(listResult):
+def print_results(listResult):
+    if (len(listResult) == 0):
+        print("Sem resultados para a palavra inserida!\n")
+
     for item in listResult[0:20]:
-        print(f"{item[0]} ::: {item[1]}")
+        print(f"{item[0]} ::: {item[1]}\n")
+
 
 def delete_little_words(text):
     stringTemp = ""
     for word in text.split(" "):
         if (len(word) > 3):
             stringTemp += word + " "
+    return stringTemp
 
-    return stringTemp;
+
+def add_tables_to_array(root):
+    for page in root:
+        table = get_table_page(page)
+        arrayTablesPages.append(table)
 
 
-cache = {}
+add_tables_to_array(root)
+
 while True:
     search = input("Faça uma busca: ").upper()
 
     if search in cache:
-       printResults(cache[search])
+       print_results(cache[search])
 
     else:
         listResult = []
-        for page in root:
-            result = count_occurrences(search, page)
-            if (result != 0):
-                title = delete_little_words(page[1].text)
-                listTemp = [result, title]
+        indexPage = 0
+
+        for table in arrayTablesPages:
+            if (search in table):
+                title = delete_little_words(root[indexPage][1].text)
+                search_occurrences = table[search]
+                listTemp = [search_occurrences, title]
                 listResult.append(listTemp)
+            indexPage += 1
 
         listResult.sort(key=lambda list: list[0], reverse=True)
-
-        if (len(listResult) == 0):
-            listResult.append("Sem resultados para a palavra inserida!")
-
         cache[search] = listResult
-
-        printResults(cache[search])
+        print_results(cache[search])
 
     question = input("Deseja fazer mais uma busca? [1] -> SIM ")
 
     if question != "1":
-        break;
+        break
