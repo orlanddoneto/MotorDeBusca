12125de704363597142c6be891316b4c4f8ffe1a
orlanddoneto <neto020735@gmail.com>
2023-10-15 01:39:05 -0300

    ajuste na busca com duas palavras e adição do ID

--- motor.py
+++ motor.py
@@ -1,3 +1,9 @@
+"""
+ORLANDO ALVES DE ALENCAR NETO
+2021017630
+HIPERMIDIA - TRAB 1
+"""
+
 import xml.etree.ElementTree as ET
 from collections import defaultdict
 import time
@@ -16,27 +22,28 @@ def count_consecutive_positions(list1, list2):
     return count * 10
 
 def search_one_word(search):
-    listResult = []
+    listResult = [] #[id, title, search_occurrences]
     indexPage = 0
 
     for table in arrayTablesPages:
         if (search in table):
-            title = delete_little_words(root[indexPage][1].text)
+            title = root[indexPage][1].text
             search_occurrences = table[search][0]
-            listTemp = [search_occurrences, title]
+            id = table[search][2]
+            listTemp = [id, title, search_occurrences]
             listResult.append(listTemp)
         indexPage += 1
     return listResult
 
 
 def search_two_words(search):
-    listResult = []
+    listResult = [] #[id, title, search_occurrences]
     indexPage = 0
     array_words = search.split(" ")
 
     for table in arrayTablesPages:
         if ((array_words[0] in table) and (array_words[1] in table)):
-            title = delete_little_words(root[indexPage][1].text)
+            title = root[indexPage][1].text
             #soma em search_occurrences as ocorrencias das duas palavras, individualmente
             search_occurrences = table[array_words[0]][0]
             search_occurrences += table[array_words[1]][0]
@@ -50,23 +57,37 @@ def search_two_words(search):
             count_search = count_consecutive_positions(index_list1, index_list2)
             search_occurrences += count_search
 
-            listTemp = [search_occurrences, title]
+            id = table[array_words[0]][2]
+            listTemp = [id, title, search_occurrences]
+            listResult.append(listTemp)
+        elif((array_words[0] in table) and (array_words[1] not in table)):
+            title = root[indexPage][1].text
+            search_occurrences = table[array_words[0]][0]
+            id = table[array_words[0]][2]
+            listTemp = [id, title, search_occurrences]
+            listResult.append(listTemp)
+        elif ((array_words[0] not in table) and (array_words[1] in table)):
+            title = root[indexPage][1].text
+            search_occurrences = table[array_words[1]][0]
+            id = table[array_words[1]][2]
+            listTemp = [id, title, search_occurrences]
             listResult.append(listTemp)
         indexPage += 1
     return listResult
 
 
 def get_table_page (page):
-    #agora, a tabela é dada com o seguinte valor: [ocorrencias, posição das ocorrencias]
-    tablePage = defaultdict(lambda: [0, []])
+    #agora, a tabela é dada com o seguinte valor: [ocorrencias, posição das ocorrencias, id]
+    tablePage = defaultdict(lambda: [0, [],0])
     for indexWord, wordInText in enumerate(page[2].text.split(" ")):
         if(len(wordInText) > 3):
             tablePage[wordInText.upper()][0] += 1
             tablePage[wordInText.upper()][1].append(indexWord)
-
+            tablePage[wordInText.upper()][2] = page.find('id').text
     for wordInTitle in page[1].text.split(" "):
         if (len(wordInTitle) > 3):
             tablePage[wordInTitle.upper()][0] += 10
+            tablePage[wordInTitle.upper()][2] = page.find('id').text
     return dict(tablePage)
 
 
@@ -75,15 +96,7 @@ def print_results(listResult):
         print("Sem resultados para a palavra inserida!\n")
 
     for item in listResult[0:20]:
-        print(f"{item[0]} ::: {item[1]}\n")
-
-
-def delete_little_words(text):
-    stringTemp = ""
-    for word in text.split(" "):
-        if (len(word) > 3):
-            stringTemp += word + " "
-    return stringTemp
+        print(f"ID ({item[0]}) ::: {item[1]} ::: OCORRÊNCIAS ({item[2]})\n")
 
 
 def add_tables_to_array(root):
@@ -109,8 +122,8 @@ while True:
             listResult = search_one_word(search)
         else:
             listResult = search_two_words(search)
-
-        listResult.sort(key=lambda list: list[0], reverse=True)
+        # [id, title, search_occurrences]
+        listResult.sort(key=lambda list: list[2], reverse=True)
         cache[search] = listResult
         print_results(cache[search])
 
